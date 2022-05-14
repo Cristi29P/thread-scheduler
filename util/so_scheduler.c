3522b09813ec47473930944b25210059ff969040
Cristian Paris <cristian.tanasep@gmail.com>
2022-05-14 12:52:42 +0300

    Added some comments

--- util/so_scheduler.c
+++ util/so_scheduler.c
@@ -1,10 +1,11 @@
 #include <semaphore.h>
+
 #include "so_scheduler.h"
 #include "prio_queue.h"
 
 #define SO_FAIL -1
 
-/* Enum representing the possible states a thread can be */
+/* Enum representing the possible states a thread can find itself in */
 typedef enum {
         READY,
         RUNNING,
@@ -12,6 +13,7 @@ typedef enum {
         TERMINATED
 } thread_state_t;
 
+/* Thread wrapper */
 typedef struct {
     tid_t tid; /* Pthread id */
     so_handler *handler; /* Function handler */
@@ -23,12 +25,13 @@ typedef struct {
     sem_t running; /* Used for blocking a thread when it is preempted */
 } thread_t;
 
+/* Scheduler info */
 typedef struct {
     int time_quantum; /* Max allowed time quantum */
     int io; /* Max number of io devices */
     int no_threads; /* Number of threads handled by the scheduler */
 
-    thread_t *current_thread; /* Pointer to the currently running thread */
+    thread_t *thread; /* Pointer to the currently running thread */
     prio_queue_t *ready; /* Threads which are waiting to be planned */
     prio_queue_t *finished; /* Threads which finished their job and are waiting to be free'd */
     prio_queue_t **waiting; /* Blocked threads by an event */
@@ -45,47 +48,53 @@ void plan_next();
 
 void scheduler_check();
 
+/* Cmp func used by the prio_queue for inserting the elements */
 int cmp_func(const void *t1, const void *t2)
 {
     return ((thread_t *)t1)->priority - ((thread_t *)t2)->priority;
 }
 
+/* Free func used by the prio_queue for freeing up the memory used by a thread */
 void free_func(void *t)
 {
+    /* Wait for the thread to finish and free the semaphore memory */
     DIE(pthread_join(((thread_t *)t)->tid, NULL), "pthread_join failed!");
     DIE(sem_destroy(&((thread_t *)t)->running), "sem_destroy failed!");
 
     free(t);
 }
 
+/* Gets the next ready thread from the queue and sets its state to RUNNING */
 void plan_next() 
 {
-    thread_t *thread = queue_pop(scheduler->ready);
-
-    thread->state = RUNNING;
-    thread->time_quantum = scheduler->time_quantum;
-    scheduler->current_thread = thread;
-
-    DIE(sem_post(&thread->running), "sem_post failed!");
+    scheduler->thread = queue_pop(scheduler->ready);
+    scheduler->thread->state = RUNNING;
+    scheduler->thread->time_quantum = scheduler->time_quantum;
+    /* Signal the thread it is okay to start execution */
+    DIE(sem_post(&scheduler->thread->running), "sem_post failed!");
 }
 
+/* Scheduling logic function. It handles all the possible cases */
 void scheduler_check() 
 {
-    thread_t *current = scheduler->current_thread;
+    thread_t *current = scheduler->thread;
 
     if (!queue_size(scheduler->ready)) {
+        /* If last thread finished, signal the scheduler to stop execution */
         if (current->state == TERMINATED)   
             DIE(sem_post(&scheduler->end), "sem_post failed!");
-
+        /* If thread did not finish execution, let it run till it ends */
         DIE(sem_post(&current->running), "sem_post failed!");
         return;
     }
 
+    /* If no currently running thread or current thread is blocked */
     if (!current || current->state == WAITING) {
         plan_next();
         return;
     }
 
+    /* */
     if (current->state == TERMINATED) {
         queue_push(scheduler->finished, current);
         plan_next();
@@ -97,14 +106,13 @@ void scheduler_check()
         plan_next();
         return;
     }
-    // POATE COMBIN ULTIMELE DOUA IF-URI
+
     if (!current->time_quantum) {
         if (current->priority == ((thread_t *)queue_top(scheduler->ready))->priority) {
             mark_as_ready(current);
             plan_next();
             return;
         }
-
         current->time_quantum = scheduler->time_quantum;
     } 
 
@@ -122,16 +130,14 @@ int so_init(unsigned int time_quantum, unsigned int io)
     if (scheduler || (io > SO_MAX_NUM_EVENTS) || !time_quantum)
         return SO_FAIL;
 
-    scheduler = calloc(1, sizeof(scheduler_t));
-    DIE(!scheduler, "Failed to initialize the scheduler!");
+    DIE(!(scheduler = calloc(1, sizeof(scheduler_t))), "scheduler calloc!");
+    DIE(sem_init(&scheduler->end, 0, 0), "sem_init failed!");
 
     scheduler->time_quantum = time_quantum;
     scheduler->io = io;
-
-    DIE(sem_init(&scheduler->end, 0, 0), "sem_init failed!");
-
     scheduler->ready = queue_init(cmp_func, free_func);
     scheduler->finished = queue_init(cmp_func, free_func);
+
     scheduler->waiting = calloc(io, sizeof(prio_queue_t *));
     DIE(!scheduler->waiting, "Failed to calloc array of waiting queues!");
 
@@ -143,14 +149,12 @@ int so_init(unsigned int time_quantum, unsigned int io)
 
 void *start_thread(void *args)
 {
-    thread_t *thread = (thread_t *)args;
-
     /* The thread should block here and wait until has the right to execute */
-    DIE(sem_wait(&thread->running), "Mutex lock thread running error.");
+    DIE(sem_wait(&((thread_t *)args)->running), "sem_wait failed!.");
     /* Thread runs its tasks via handler */
-    thread->handler(thread->priority);
+    ((thread_t *)args)->handler(((thread_t *)args)->priority);
     /* Thread finished its tasks. Mark the thread as done. */
-    thread->state = TERMINATED;
+    ((thread_t *)args)->state = TERMINATED;
     /* Call the scheduler and add thread to finished queue */
     scheduler_check();
 
@@ -161,12 +165,10 @@ tid_t so_fork(so_handler *func, unsigned int priority)
 {
     thread_t *thread;
 
-    if (!func || priority > SO_MAX_PRIO) {
+    if (!func || priority > SO_MAX_PRIO)
         return INVALID_TID;
-    }
 
-    thread = calloc(1, sizeof(thread_t));
-    DIE(!thread, "Thread calloc failed!");
+    DIE(!(thread = calloc(1, sizeof(thread_t))), "thread calloc failed!");
 
     thread->priority = priority;
     thread->time_quantum = scheduler->time_quantum;
@@ -176,11 +178,10 @@ tid_t so_fork(so_handler *func, unsigned int priority)
     DIE(pthread_create(&thread->tid, NULL, start_thread, thread), "pthread_create failed!");
     
     ++(scheduler->no_threads);
-
     mark_as_ready(thread);
 
     /* If we are the first thread */
-    if (scheduler->current_thread != NULL)
+    if (scheduler->thread != NULL)
         so_exec();
     else
         scheduler_check();
@@ -193,8 +194,8 @@ int so_wait(unsigned int io)
     if ((int)io >= scheduler->io)
         return SO_FAIL;
 
-    scheduler->current_thread->state = WAITING;
-    queue_push(scheduler->waiting[io], scheduler->current_thread);
+    scheduler->thread->state = WAITING;
+    queue_push(scheduler->waiting[io], scheduler->thread);
 
     so_exec();
     return 0;
@@ -203,14 +204,14 @@ int so_wait(unsigned int io)
 int so_signal(unsigned int io)
 {
     thread_t *aux;
-    int cnt = 0;
+    int cnt;
+
     if ((int)io >= scheduler->io)
         return SO_FAIL;
 
-    while ((aux = queue_pop(scheduler->waiting[io]))) {
+    /* Wake-up all the threads waiting for that specific io */
+    for (cnt = 0; (aux = queue_pop(scheduler->waiting[io])); ++cnt)
         mark_as_ready(aux);
-        ++cnt;
-    }
 
     so_exec();
     return cnt;
@@ -218,12 +219,15 @@ int so_signal(unsigned int io)
 
 void so_exec(void)
 {
-    /* Decrease the time remaining on the processor */
-    --(scheduler->current_thread->time_quantum);
+    thread_t *current = scheduler->thread;
+
+    --(current->time_quantum);
+
     /* Call the scheduler */
     scheduler_check();
-    /* Stay here if you get preempteed */
-    DIE(sem_wait(&scheduler->current_thread->running), "sem_wait failed!");
+
+    /* Wait here if you get preempteed */
+    DIE(sem_wait(&current->running), "sem_wait failed!");
 }
 
 void so_end(void)
@@ -237,8 +241,8 @@ void so_end(void)
     queue_free(scheduler->ready);
     queue_free(scheduler->finished);
 
-    if (scheduler->current_thread)
-        free_func(scheduler->current_thread);
+    if (scheduler->thread)
+        free_func(scheduler->thread);
     
     for (int i = 0; i != (int)scheduler->io; ++i)
         queue_free(scheduler->waiting[i]);
